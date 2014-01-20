de32bed4dd9cf11583b77459d0a695529f33777a
rubytester <github@rubytester.com>
2014-01-19 21:46:13 -0600

    ahem. we should return driver from session cache
    no need to start it again. maintain session driver

--- lib/selerki/driver.rb
+++ lib/selerki/driver.rb
@@ -1,8 +1,37 @@
+require 'singleton'
+
 module Selerki
+
   module Driver
-    def self.start
-      Selenium::WebDriver.for :chrome
+
+    class << self
+
+      # current driver for talking to the browser
+      def driver
+        cache.driver
+      end
+
+      # private
+      def cache
+        Cache.instance
+      end
     end
 
+    private
+
+    # Private Cache were we store drivers once started.
+    class Cache
+      include Singleton
+      attr_accessor :driver
+
+      def driver
+        @driver ||= start
+      end
+
+      def start
+        Selenium::WebDriver.for :chrome
+      end
+    end
   end
+
 end
--- spec/driver_spec.rb
+++ spec/driver_spec.rb
@@ -2,8 +2,21 @@ require 'spec_helper'
 
 describe Selerki::Driver do
 
-  it 'start' do
-    wd = Selerki::Driver.start
+  context 'when no drivers present in the system' do
+
+    before do
+      Selerki::Driver.cache.driver = nil
+    end
+
+    it '.driver gives us webdriver' do
+      wd = Selerki::Driver.driver
+    end
+
+    it 'asking for driver again we should get the same driver from cache and not new' do
+      wd = Selerki::Driver.driver
+      wd.should eq(Selerki::Driver.driver)
+    end
+
   end
 
 end
